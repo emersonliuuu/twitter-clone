dd833941ab0ed0eee49df0736a385f824b040037
emerson liu <emerson199744@gmail.com>
2023-02-03 01:29:31 +0800

    Add sign in function

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -3,6 +3,8 @@ import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import User from "../models/User";
 
+const DEFAULT = "h5oNhVmLhM2UreaZLsLLF3H";
+
 export const signup = async (
   req: Request,
   res: Response,
@@ -18,13 +20,45 @@ export const signup = async (
     // @ts-ignore
     const { password, ...savedUserData } = newUser._doc; // TODO
 
-    const _ = process.env.JWT ?? "h5oNhVmLhM2UreaZLsLLF3H";
+    const _ = process.env.JWT ?? DEFAULT;
     const token = jwt.sign({ id: newUser._id, pwd: password }, _);
 
     res
       .cookie("access_token", token, { httpOnly: true })
       .status(200)
       .send(savedUserData);
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const signin = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const user = await User.findOne({ username: req.body.username });
+    if (!user) {
+      res.status(401);
+      return next(new Error("username or password incorrect"));
+    }
+
+    const isCorrect = bcrypt.compareSync(req.body.password, user?.password!);
+    if (!isCorrect) {
+      res.status(401);
+      return next(new Error("username or password incorrect"));
+    }
+
+    // @ts-ignore
+    const { password, ...userData } = user._doc;
+    const _ = process.env.JWT ?? DEFAULT;
+    const token = jwt.sign({ id: user._id, pwd: password }, _);
+
+    res
+      .cookie("access_token", token, { httpOnly: true })
+      .status(200)
+      .json(userData);
   } catch (err) {
     next(err);
   }
--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,8 +1,9 @@
 import express from "express";
-import { signup } from "../controllers/users";
+import { signin, signup } from "../controllers/users";
 
 const router = express.Router();
 
 router.post("/signup", signup);
+router.post("/signin", signin);
 
 export default router;

31a9016109f17a9d067c40eb1d20fd4034ac3d33
emerson liu <emerson199744@gmail.com>
2023-02-04 09:15:22 +0800

    Add likeCount field

--- src/models/Tweet.ts
+++ src/models/Tweet.ts
@@ -1,19 +1,26 @@
 import mongoose from "mongoose";
 
-const TweetSchema = new mongoose.Schema({
-  userId: {
-    type: String,
-    require: true,
+const TweetSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: String,
+      require: true,
+    },
+    description: {
+      type: String,
+      max: 280,
+      require: true,
+    },
+    likes: {
+      type: Array,
+      default: [],
+    },
+    likesCount: {
+      type: Number,
+      default: 0,
+    },
   },
-  description: {
-    type: String,
-    max: 280,
-    require: true,
-  },
-  likes: {
-    type: Array,
-    default: [],
-  },
-});
+  { timestamps: true }
+);
 
 export default mongoose.model("Tweet", TweetSchema);
